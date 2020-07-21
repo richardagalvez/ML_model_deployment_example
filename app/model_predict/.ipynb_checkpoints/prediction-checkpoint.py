ab14aec073c987ebf74de003badf915f4e2e1738
Richard Galvez <richard.galvez@anheuser-busch.com>
2020-07-21 14:25:44 -0400

    initial commit

--- app/model_predict/.ipynb_checkpoints/prediction-checkpoint.py
+++
@@ -1,5 +0,0 @@
-import h2o
-def predict(input, models: list):
-    Y1_prediction = models[0].predict(input).flatten()
-    Y2_prediction = models[1].predict(input).flatten()
-    return Y1_prediction, Y2_prediction
